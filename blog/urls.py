9c5fd06c21625fc81deb0d435c872faa7946d3ec
azkazaskans <azkasavanti@gmail.com>
2017-05-13 17:02:33 +0200

    Added view and template for detail led blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ from . import views
 #this pattern tells Django that views.post_list is the right place to go if someone enters the website
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+
 ]
